9d645b30934bc3de92552d1fdda9ad35ac823e47
Mahadevi16 <Mahadeviraj97@gmail.com>
2022-11-19 09:37:23 +0530

    commit

---
+++ Automation/src/main/java/school/Student.java
@@ -0,0 +1,24 @@
+package school;
+
+public class Student {
+    private void add() {
+    	int a=90, b=4, c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+    private void add1() {
+    	int a=90, b=4, c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+	public static void main(String args[]) {
+		
+		Student test = new Student();
+		test.add();
+		test.add1();
+		
+	}
+
+}
---
+++ Automation/src/main/java/school/Testing.java
@@ -0,0 +1,14 @@
+package school;
+
+public class Testing {
+    private void display() {
+		System.out.println("Hello");
+
+	}
+	public static void main(String[] args) {
+		Testing obj=new Testing();
+		obj.display();
+
+	}
+
+}
